d954de8d50d3aefed93407827555e9375fc945de
Samir Dumre <samirdumre234@gmail.com>
2024-10-01 20:18:15 +0545

    deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+    return (
+        <div className="card">
+            <Avatar imgSrc={"/photouse.jpg"} altText={"My photo"}/>
+            <div className="data">
+                <Intro heading={"Samir Dumre"}
+                       data={"Frontend developer, Video Editor and CSIT Student. My hobbies are hiking, cycling and playing games. I live to exercise daily."} />
+                {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+                <div className={"skill-list"}>
+                <SkillList skill={"HTML + CSS"} emoji={"🦾"} backgroundColor={"#b1b1b1"} />
+                <SkillList skill={"JavaScript"} emoji={"💪"} backgroundColor={"violet"} />
+                <SkillList skill={"Git & Github"} emoji={"🦾"} backgroundColor={"cyan"} />
+                <SkillList skill={"React"} emoji={"🦾"} backgroundColor={"skyblue"} />
+                <SkillList skill={"Video Editing"} emoji={"🦾"} backgroundColor={"gray"} />
+                <SkillList skill={"Video Editing"} emoji={"🦾"} backgroundColor={"gold"} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Avatar(props) {
+    return (
+        <img className={"avatar"} src={props.imgSrc} alt={props.altText}/>
+    )
+}
+
+function Intro(props) {
+    return (
+        <span>
+            <h1>{props.heading}</h1>
+            <p>{props.data}</p>
+        </span>
+    )
+}
+
+function SkillList(props) {
+    const skillStyle = {
+        backgroundColor: props.backgroundColor
+    }
+    return (
+            <p style={skillStyle} className={"skill"}>{props.skill} {props.emoji}</p>
+    )
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <App/>
+    </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
